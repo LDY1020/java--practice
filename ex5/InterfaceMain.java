0b867be3e388807c86561bf2320601b50177380c
LDY1020 <164770190+LDY1020@users.noreply.github.com>
2025-08-25 15:15:17 +0900

    인터페이스

---
+++ ex5/InterfaceMain.java
@@ -0,0 +1,36 @@
+package poly.ex5;
+
+import poly.ex4.AbstractAnimal;
+
+public class InterfaceMain {
+
+    public static void main(String[] args) {
+        //  InterfaceAnimal interfaceAnimal = new InterfaceAnimal() 추상이라 객체 생성불가
+
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+        Cow cow = new Cow();
+
+        moveAnimal(dog);
+        moveAnimal(cat);
+        moveAnimal(cow);
+
+
+        soundAnimal(dog);
+        soundAnimal(cat);
+        soundAnimal(cow);
+    }
+
+    //변하지 않는 부분
+    private static void moveAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 이동 테스트 시작");
+        animal.move();
+        System.out.println("동물 이동 테스트 종료");
+    }
+
+    private static void soundAnimal(InterfaceAnimal animal) {
+        System.out.println("동물 소리 테스트 시작");
+        animal.sound();
+        System.out.println("동물 소리 테스트 종료");
+    }
+}
