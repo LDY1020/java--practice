ca8f3b5337daeee59e9a8ff04795a04ff108bea7
LDY1020 <164770190+LDY1020@users.noreply.github.com>
2025-09-27 19:47:16 +0900

    String - 자주쓰는것

---
+++ test/TestString4.java
@@ -0,0 +1,12 @@
+package Lang.string.test;
+
+public class TestString4 {
+
+    public static void main(String[] args) {
+        String str = "hello.txt";
+        String filename = str.substring(0 , 5);
+        System.out.println("filename = " + filename);
+        String endname = str.substring(0 , 5);
+
+    }
+}
---
+++ test/TestString9.java
@@ -0,0 +1,17 @@
+package Lang.string.test;
+
+public class TestString9 {
+
+    public static void main(String[] args) {
+        String email = "hello@example.com";
+
+        String[] parts = email.split("@");
+        String idPart = parts[0];
+        String domainPart = parts[1];
+
+        System.out.println("domainPart = " + domainPart);
+        System.out.println("idPart = " + idPart);
+
+
+    }
+}
